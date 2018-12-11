499ac3998dc9958a33948cff7b8f6d268459b2ed
ksopyla <sopyla@ermlab.com>
2018-12-11 07:15:19 +0100

    add scipy uniform distribution to param dict, change GridSearch to Randomized, not tested yet

--- svm_mnist_random_search.py
+++ svm_mnist_random_search.py
@@ -12,7 +12,7 @@ import datetime as dt
 
 
 from time import time
-from scipy.stats import randint as sp_randint
+
 
 
 from sklearn.model_selection import RandomizedSearchCV
@@ -68,44 +68,39 @@ X_train, X_test, y_train, y_test = train_test_split(X_data, Y, test_size=0.15, r
 # Warning! It takes really long time to compute this about 2 days
 
 # Create parameters grid for RBF kernel, we have to set C and gamma
-from sklearn.model_selection import GridSearchCV
 
-# generate matrix with all gammas
-# [ [10^-4, 2*10^-4, 5*10^-4], 
-#   [10^-3, 2*10^-3, 5*10^-3],
-#   ......
-#   [10^3, 2*10^3, 5*10^3] ]
-#gamma_range = np.outer(np.logspace(-4, 3, 8),np.array([1,2, 5]))
-gamma_range = np.outer(np.logspace(-3, 0, 4),np.array([1,5]))
-gamma_range = gamma_range.flatten()
+from scipy.stats import randint as sp_randint
+from scipy.stats import uniform as sp_uniform
 
-# generate matrix with all C
-#C_range = np.outer(np.logspace(-3, 3, 7),np.array([1,2, 5]))
-C_range = np.outer(np.logspace(-1, 1, 3),np.array([1,5]))
-# flatten matrix, change to 1D numpy array
-C_range = C_range.flatten()
+C_range = sp_uniform(scale=10)
+gamma_range = sp_uniform(scale=1)
+parameters = {'kernel':['rbf'],
+              'C':C_range, 
+              'gamma': gamma_range
+ }
 
-parameters = {'kernel':['rbf'], 'C':C_range, 'gamma': gamma_range}
-
+n_iter_search = 10
 svm_clsf = svm.SVC()
-grid_clsf = GridSearchCV(estimator=svm_clsf,param_grid=parameters,n_jobs=1, verbose=2)
-
+rnd_clsf = RandomizedSearchCV(estimator=svm,
+                              param_distributions=parameters,
+                              n_iter=n_iter_search, cv=5,
+                              verbose=2)
 
 start_time = dt.datetime.now()
 print('Start param searching at {}'.format(str(start_time)))
 
-grid_clsf.fit(X_train, y_train)
+rnd_clsf.fit(X_train, y_train)
 
 elapsed_time= dt.datetime.now() - start_time
 print('Elapsed time, param searching {}'.format(str(elapsed_time)))
-sorted(grid_clsf.cv_results_.keys())
+sorted(rnd_clsf.cv_results_.keys())
 
-classifier = grid_clsf.best_estimator_
-params = grid_clsf.best_params_
+classifier = rnd_clsf.best_estimator_
+params = rnd_clsf.best_params_
 
 
 
-scores = grid_clsf.cv_results_['mean_test_score'].reshape(len(C_range),
+scores = rnd_clsf.cv_results_['mean_test_score'].reshape(len(C_range),
                                                      len(gamma_range))
 
 plot_param_space_scores(scores, C_range, gamma_range)
